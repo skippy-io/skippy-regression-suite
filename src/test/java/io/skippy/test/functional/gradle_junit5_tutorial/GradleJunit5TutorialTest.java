391ac7223f2ef7694ab8459c588f07ed71fac655
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2023-11-26 22:11:28 -0600

    issue/14: Add license header and copyright note to all source files

--- src/test/java/io/skippy/test/functional/gradle_junit5_tutorial/GradleJunit5TutorialTest.java
+++ src/test/java/io/skippy/test/functional/gradle_junit5_tutorial/GradleJunit5TutorialTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.skippy.test.functional.gradle_junit5_tutorial;
 
 import io.skippy.test.SkippyVersion;
@@ -15,6 +31,11 @@ import static java.util.regex.Pattern.quote;
 import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+/**
+ * Functional test for the Getting Started With Gradle & JUnit 5 Tutorial.
+ *
+ * @author Florian McKee
+ */
 public class GradleJunit5TutorialTest {
 
     @Test
