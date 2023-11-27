fd24c118621d1ef21e1c349eae09c816d7b619ad
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2023-11-26 22:16:07 -0600

    issue/14: Add license header and copyright note to all source files (#1)

--- src/test/java/io/skippy/test/issues/issue0001/Issue0001Test.java
+++ src/test/java/io/skippy/test/issues/issue0001/Issue0001Test.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.skippy.test.issues.issue0001;
 
 import io.skippy.test.SkippyVersion;
@@ -11,6 +27,11 @@ import java.nio.file.Files;
 import static java.util.regex.Pattern.quote;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+/**
+ * Functional test for https://github.com/skippy-io/skippy/issues/1.
+ *
+ * @author Florian McKee
+ */
 public class Issue0001Test {
 
     @Test
