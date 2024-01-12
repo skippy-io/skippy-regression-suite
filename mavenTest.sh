317c4e9aba2d6612b5eabf43a2df5d4683b0fd0d
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-01-11 19:48:38 -0600

    support for Maven-based functional tests

--- mavenTest.sh
+++ mavenTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./gradlew clean processTestResources
-mvn -f src/test/resources/test-projects -P sub-modules-only verify -DskippyAnalyze=true
-mvn test
+mvn -f src/test/resources/test-projects verify -DskippyAnalyze=true -P sub-modules-only --no-transfer-progress
+mvn test --no-transfer-progress
