f7f7447a6dd2fea94a9ba2241a36cc87e3934535
Florian McKee <84742327+fmck3516@users.noreply.github.com>
2024-01-13 08:22:25 -0600

    support for Maven-based functional tests

--- mavenTest.sh
+++ mavenTest.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-./gradlew clean processTestResources
-mvn -f src/test/resources/test-projects verify -DskippyAnalyze=true -P sub-modules-only --no-transfer-progress
-mvn test --no-transfer-progress
+
+./gradlew processTestResources
+
+baseDir=src/test/resources/test-projects
+
+mvn -f $baseDir/junit5-smoketest                      verify -DskippyAnalyze=true --no-transfer-progress
+mvn -f $baseDir/junit5-no-skippy-plugin               verify -DskippyAnalyze=true --no-transfer-progress
+mvn -f $baseDir/junit4-smoketest                      verify -DskippyAnalyze=true --no-transfer-progress
+mvn -f $baseDir/junit4-no-skippy-plugin               verify -DskippyAnalyze=true --no-transfer-progress
+mvn -f $baseDir/skippy-clean-existing-skippy-folder   skippy:clean                --no-transfer-progress
+mvn -f $baseDir/skippy-clean-no-skippy-folder         skippy:clean                --no-transfer-progress
+
+mvn clean test --no-transfer-progress
---
+++ src/test/java/io/skippy/test/maven/SkippyCleanNoSkippyFolderTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.skippy.test.maven;
+
+import io.skippy.test.SkippyTestTag;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests behavior of mvn skippy:clean for a project without skippy folder.
+ *
+ * @author Florian McKee
+ */
+public class SkippyCleanNoSkippyFolderTest {
+
+    @Test
+    @Tag(SkippyTestTag.MAVEN)
+    public void testBuild() throws Exception {
+        var projectDir = new File(getClass().getResource("/test-projects/skippy-clean-no-skippy-folder").toURI());
+        assertEquals(false, projectDir.toPath().resolve("skippy").toFile().exists());
+    }
+
+}
